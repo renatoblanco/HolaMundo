21568727399d025fa94a9676f9519a4ce8b2b009
renatoblanco <pablorenato2@gmail.com>
2015-09-30 16:41:21 -0300

    c

--- app/src/main/java/uy/com/lifan/holamundo/SaludoActivity.java
+++ app/src/main/java/uy/com/lifan/holamundo/SaludoActivity.java
@@ -89,7 +89,7 @@ public class SaludoActivity extends AppCompatActivity {
             }
         }catch (Exception ex){
             //falta capturar la excepcion
-            db=null;
+            
         }
         return lista;
 
