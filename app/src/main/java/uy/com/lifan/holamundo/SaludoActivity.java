9dbd9ee06772e48c0a3e278772d46b08da4d575b
renatoblanco <pablorenato2@gmail.com>
2015-10-01 09:17:15 -0300

    prueba

--- app/src/main/java/uy/com/lifan/holamundo/SaludoActivity.java
+++ app/src/main/java/uy/com/lifan/holamundo/SaludoActivity.java
@@ -90,6 +90,7 @@ public class SaludoActivity extends AppCompatActivity {
         }catch (Exception ex){
 //aca me falta capturar la excpcion
             //no comitea
+            
         }
         return lista;
 
